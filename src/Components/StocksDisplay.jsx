faf6140ee4ee92dffb25b01356c0276cd039468c
Shivam Verma <shivamv2131@gmail.com>
2025-06-17 01:43:30 +0530

    stocks display

--- src/Components/StocksDisplay.jsx
+++ src/Components/StocksDisplay.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import Bar from "./StocksPage/bar";
 
 function StocksDisplay(){
     return(
         <div className="w-screen h-[86vh] bg-zinc-900">
+            <hr className="text-zinc-600"/>
+            <Bar />
+            <hr className="text-zinc-600"/>
         </div>
     )
 }
