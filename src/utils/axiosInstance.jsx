f9a066478db33f317ff420e9ab472d9b8019bb3f
Shivam Verma <shivamv2131@gmail.com>
2025-06-17 04:40:14 +0530

    axios fix

--- src/utils/axiosInstance.jsx
+++ src/utils/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL,
+    baseURL: "fingenius-back-nn7wvmqpz-shivamv2209s-projects.vercel.app/api",
     headers: {
         "Content-Type": "application/json",
     },
